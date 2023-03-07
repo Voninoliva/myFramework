8867fb5c66f967b02c6357e6bcacbcd17c6f29e3
Voninoliva <voninolivam@gmail.com>
2023-03-07 07:39:38 +0300

    sprint2Commit

---
+++ java/etu1790/framework/Mapping.java
@@ -0,0 +1,5 @@
+package etu1790.framework;
+public class Mapping {
+    String className;
+    String Method;
+}
--- java/etu1790/framework/servlet/FrontServlet.java
+++ java/etu1790/framework/servlet/FrontServlet.java
@@ -1,5 +1,6 @@
 package etu1790.framework.servlet;
 
+import etu1790.framework.Mapping;
 import fonctions.Utils;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,9 +8,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 
 public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> mappingsUrls;
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
