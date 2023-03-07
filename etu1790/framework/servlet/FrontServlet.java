5502521617d5734493179ae38b74423a3da7da50
Voninoliva <voninolivam@gmail.com>
2023-03-07 06:45:06 +0300

    rightSprint1

--- etu1790/framework/servlet/FrontServlet.java
+++ etu1790/framework/servlet/FrontServlet.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fonctions;
+package etu1790.framework.servlet;
 
+import fonctions.Utils;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,10 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author nec
- */
+
 public class FrontServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
